0177bc0dfa675e34175ed054b8269f4dee9e4682
TimSherlock <shang6466575@163.com>
2015-02-02 17:10:51 +0800

    UserDaoImpl 中方法的实现完成

--- Server/WeChat/src/com/wechat/dao/UserDao.java
+++ Server/WeChat/src/com/wechat/dao/UserDao.java
@@ -1,10 +1,22 @@
 package com.wechat.dao;
 
+import java.sql.SQLException;
+
+import com.wechat.entity.User;
+
 public interface UserDao {
-	
+
+	/**
+	 * 
+	 * @param userId
+	 * @return
+	 */
 	public boolean checkIdUnique(String userId);
-	
-	public boolean Login(String userId, String password);
-	
-	public boolean addUser(String userId, String username, String password, String icon);
+
+	public User getUser(String userId, String password);
+
+	public boolean addUser(String userId, String username, String password,
+			String icon);
+
+	public boolean modifyUserIcon(String userId, String icon);
 }
--- Server/WeChat/src/com/wechat/dao/impl/UserDaoImpl.java
+++ Server/WeChat/src/com/wechat/dao/impl/UserDaoImpl.java
@@ -11,30 +11,148 @@ import com.wechat.tool.ReadProperties;
 import com.wechat.dao.UserDao;
 import com.wechat.entity.User;
 
-public class UserDaoImpl implements UserDao{
-	
+public class UserDaoImpl implements UserDao {
+
 	private QueryRunner queryRunner = new QueryRunner();
-	
+
+	/*
+	 * (non-Javadoc) user Login OK
+	 * 
+	 * @see com.wechat.dao.UserDao#Login(java.lang.String, java.lang.String)
+	 */
 	@Override
-	public boolean Login(String userId, String password) {
+	public User getUser(String userId, String password) {
 		Connection conn = (Connection) C3P0DBConnectionPool.getConnection();
-		
+		User user = null;
 		try {
-			queryRunner.query(conn, ReadProperties.read("sql", "getUser"), new BeanHandler<>(User.class), userId, password);
+			user = queryRunner.query(conn, ReadProperties
+					.read("sql", "getUser"), new BeanHandler<>(User.class),
+					userId, password);
+
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return user;
+	}
+
+	/*
+	 * (non-Javadoc) add User OK
+	 * 
+	 * @see com.wechat.dao.UserDao#addUser(java.lang.String, java.lang.String,
+	 * java.lang.String, java.lang.String)
+	 */
+	@Override
+	public boolean addUser(String userId, String username, String password,
+			String icon) {
+		Connection conn = (Connection) C3P0DBConnectionPool.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			int ret = -1;
+			ret = queryRunner.update(conn,
+					ReadProperties.read("sql", "addUser"), userId, username,
+					password, icon);
+			if (ret > 0) {
+				conn.commit();
+				return true;
+			} else {
+				conn.rollback();
+			}
+
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 		}
 		return false;
 	}
 
+	/*
+	 * (non-Javadoc) OK
+	 * 
+	 * @see com.wechat.dao.UserDao#checkIdUnique(java.lang.String)
+	 */
 	@Override
-	public boolean addUser(String userId, String username, String password,
-			String icon) {
+	public boolean checkIdUnique(String userId) {
+
+		Connection conn = (Connection) C3P0DBConnectionPool.getConnection();
+		User user = null;
+		try {
+			user = queryRunner.query(conn,
+					ReadProperties.read("sql", "getUserByUserId"),
+					new BeanHandler<>(User.class), userId);
+			if (user != null) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return false;
 	}
 
+	/* (non-Javadoc) OK
+	 * @see com.wechat.dao.UserDao#modifyUserIcon(java.lang.String, java.lang.String)
+	 */
 	@Override
-	public boolean checkIdUnique(String userId) {
+	public boolean modifyUserIcon(String userId, String icon) {
+		Connection conn = (Connection) C3P0DBConnectionPool.getConnection();
+
+		try {
+			conn.setAutoCommit(false);
+			int ret = -1;
+			ret = queryRunner.update(conn,
+					ReadProperties.read("sql", "modifyUserIcon"), icon, userId);
+			if (ret > 0) {
+				conn.commit();
+				return true;
+			} else {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return false;
 	}
 
