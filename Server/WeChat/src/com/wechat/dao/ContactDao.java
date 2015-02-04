b434ad81989365c5d39de06efba5995b8eb9a3ea
brad <brad@gmail.com>
2015-02-04 09:41:14 +0800

    aa

--- Server/WeChat/src/com/wechat/dao/ContactDao.java
+++ Server/WeChat/src/com/wechat/dao/ContactDao.java
@@ -6,12 +6,31 @@ import com.wechat.entity.Contact;
 import com.wechat.entity.User;
 
 public interface ContactDao {
-	
+	/**
+	 * 
+	 * @param ownerId
+	 * @return
+	 */
 	public List<User> getContacts(String ownerId);
-	
+	/**
+	 * 
+	 * @param ownerId
+	 * @param contactId
+	 * @return
+	 */
 	public Contact getContact(String ownerId, String contactId);
-	
+	/**
+	 * 
+	 * @param ownerId
+	 * @param contactId
+	 * @return
+	 */
 	public boolean addContact(String ownerId, String contactId);
-	
+	/**
+	 * 
+	 * @param ownerId
+	 * @param contactId
+	 * @return
+	 */
 	public boolean deleteContact(String ownerId, String contactId);
 }
