ad2a12adde33b9f1d0b05337d1880dd963b0b967
brad <brad@gmail.com>
2015-02-12 16:12:11 +0800

    aa

--- Server/WeChat/src/com/wechat/dao/MessageDao.java
+++ Server/WeChat/src/com/wechat/dao/MessageDao.java
@@ -6,7 +6,16 @@ import com.wechat.entity.Message;
 
 public interface MessageDao {
 
+	/**
+	 * @param msg
+	 * @return
+	 */
 	public boolean addMessage(Message msg);
 	
+	/**
+	 * @param ownerId
+	 * @param contactId
+	 * @return
+	 */
 	public List<Message> getMessages(String ownerId, String contactId);
 }
