8ecdc0469fe514da711553b4eabb62df7b4c3275
joselyncui <15895026586@163.com>
2015-02-13 16:25:31 +0800

    change chatting logic change contact list logic

--- Client/wechat/js/utils/constant.js
+++ Client/wechat/js/utils/constant.js
@@ -1,7 +1,7 @@
 weChatApp.value('appInfo', {
     "appKey": "sfci50a7crkai",
     "token": "",
-    "basicUrl": 'http://192.168.1.78:8080/WeChat/rest/UserService/'
+    "basicUrl": 'http://192.168.1.61:8081/WeChat/rest/UserService/'
 });
 
 weChatApp.value('userInfo', {
