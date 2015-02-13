6f17bf17cbe07db54c814b26cd6012547c38c7aa
brad <brad@gmail.com>
2015-02-13 15:15:01 +0800

    aaa

---
+++ Server/WeChat/src/com/wechat/dao/impl/ContactRequestDaoImpl.java
@@ -0,0 +1,26 @@
+package com.wechat.dao.impl;
+
+import java.util.List;
+
+import com.wechat.dao.ContactRequestDao;
+import com.wechat.entity.ContactRequest;
+
+public class ContactRequestDaoImpl implements ContactRequestDao {
+
+	@Override
+	public boolean addContactRequest(ContactRequest cr) {
+		return false;
+	}
+
+	@Override
+	public boolean changeStatus(String userId, String targetId) {
+		return false;
+	}
+
+	@Override
+	public List<ContactRequest> getContactRequests(String targetId) {
+		return null;
+	}
+
+	
+}
